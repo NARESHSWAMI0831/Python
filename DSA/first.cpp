07e9f433affc14c8d83a45ee7197df98049b1f0f
Naresh Swami <nareshswami0831@gmail.com>
2025-07-20 23:40:05 +0530

    Daily push: July 20 Python work

---
+++ DSA/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int age;
+    cout<<"enter your age :"<<endl;
+    cin>>age;
+    cout<<age<<endl;
+    return 0;
+}
+// Boilerplate Code
+
---
+++ Python/Loops.py
@@ -0,0 +1,69 @@
+# loops
+
+# while loop
+
+# i =1
+# while i <=5 :
+#     print(" I can do it",i)
+#     i += 1
+
+# print(i)
+
+
+# practice while loop
+
+# 1 print 1 to 1000
+
+# num=1
+# while num<11:
+#     print(num)
+#     num+=1
+
+
+
+# 2 100 to 1
+
+# num=100
+# while num>=1:
+#     print(num)
+#     num-=1
+
+# 3 multiplication table
+
+# t=1
+# while t<=10:
+#     print(8*t)
+#     t+=1
+
+# 4     print the element
+
+# i=[1,4,16,25,36,49,90,31,81,100]
+# idx=0
+# while idx<len(i):
+#     print(i[idx])
+#     idx+=1
+
+# 5 serch of tuple
+
+i=(1,4,16,25,36,49,90,31,81,100)
+
+x=36
+z=0
+while z<len(i):
+    if(i[z]==x):
+     print("found",z)
+     break
+    else:
+       print("finding..")
+    z+=1
+
+
+
+# Break and Continue
+
+i=1
+while i<=5:
+    print(i)
+    if(i==3):
+        break
+    i+=1
