47b05e4b29b44456251417ebaf50bb09c445b670
Naresh Swami <nareshswami0831@gmail.com>
2025-07-25 00:14:04 +0530

    Daily push: July 25 Python list operations practice

--- Python/recursion.py
+++ Python/recursion.py
@@ -9,4 +9,20 @@ def show(n):
 show(5)
 
 
-# yes i can do it
+# Daily Python Practice - July 25
+
+# A list of programming languages
+languages = ["Python", "Java", "C++", "JavaScript", "Go"]
+
+# Print each language with a number
+print("Programming Languages:")
+for i, lang in enumerate(languages, start=1):
+    print(f"{i}. {lang}")
+
+# Add a new language
+languages.append("Rust")
+print("\nUpdated List:", languages)
+
+# Remove a language
+languages.remove("Java")
+print("After Removing Java:", languages)
