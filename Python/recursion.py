266047322bd3b8094b48130ba9b074faacf98b86
Naresh Swami <nareshswami0831@gmail.com>
2025-08-16 23:33:06 +0530

    Daily push: August 16 Python program (Armstrong number checker)

--- Python/recursion.py
+++ Python/recursion.py
@@ -205,3 +205,16 @@ for char in text:
     reversed_text_loop = char + reversed_text_loop
 print("Reversed (loop):", reversed_text_loop)
 
+# Armstrong number checker
+num = int(input("Enter a number: "))
+sum = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+
+if num == sum:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
